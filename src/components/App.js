574a2115d42f6c01dd7cae7859c203021e67dab6
dMuguti <omiahderek56@gmail.com>
2023-04-26 15:34:19 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Home from "./Home";
 import About from "./About";
 import Login from "./Login";
 import Navbar from "./Navbar";
-
+//app
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
